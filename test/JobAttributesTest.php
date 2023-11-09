7782112e5e903e6e2b546365bf534e1212ed7b59
Jason Quick <jasonq@brightlightbooks.com>
2023-11-09 18:32:52 -0500

    Bugfix: job-state-reasons case was missing

--- test/JobAttributesTest.php
+++ test/JobAttributesTest.php
@@ -8,4 +8,14 @@ class JobAttributeTest extends TestCase
     {
         $this->assertSame(1, 1);
     }
+
+    public function testJobStateReasonsCanBeSet() {
+
+        $jobAttributes = new \obray\ipp\JobAttributes();
+        $jobAttributes->set('job-state-reasons',new \obray\ipp\enums\JobStateReasons(\obray\ipp\enums\JobStateReasons::jobIncoming));
+
+        $this->assertEquals('jobincoming',(string) $jobAttributes->{'job-state-reasons'});
+
+    }
+
 }
