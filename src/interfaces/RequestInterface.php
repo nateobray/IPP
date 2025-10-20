569f7fa380ab904d857556f015d0403b0f4cede8
Jan Fejtek <jan.fejtek@gmail.com>
2025-10-20 11:32:49 +0200

    Update RequestInterface.php

--- src/interfaces/RequestInterface.php
+++ src/interfaces/RequestInterface.php
@@ -3,5 +3,5 @@ namespace obray\ipp\interfaces;
 
 interface RequestInterface
 {
-    public static function send(string $printerURI, string $encodedPayload, string $user=null, string $password=null): \obray\ipp\transport\IPPPayload;
+    public static function send(string $printerURI, string $encodedPayload, ?string $user=null, ?string $password=null): \obray\ipp\transport\IPPPayload;
 }
